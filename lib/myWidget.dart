0998ca1e223b1b511be50e8af8f6698324a92a8f
Rai Griffioen <raigriffioen3@gmail.com>
2020-10-23 13:44:07 +0200

    Update myWidget.dart

--- lib/myWidget.dart
+++ lib/myWidget.dart
@@ -5,7 +5,6 @@ class MyWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     double _currentSliderValue = 20;
 
-
     return Container(
       margin: const EdgeInsets.all(5),
       height: 500,
@@ -21,3 +20,5 @@ class MyWidget extends StatelessWidget {
         child: const Text('Open Page', style: TextStyle(fontSize: 20)),
       ),
     );
+  }
+}
